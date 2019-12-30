b1ef08aa4b5d1815fa6a844a94c1289b35654020
abhishek singh <abhishek.singh@instarem.com>
2019-12-30 15:37:06 +0530

    yahoo weather api working

--- service/src/routes/weather.js
+++ service/src/routes/weather.js
@@ -1,19 +1,41 @@
 let express = require('express')
-var request = require('request');
 let router = express.Router()
+let OAuth = require('oauth');
+
+
+let header = {
+    "X-Yahoo-App-Id": "hZq1iA5i"
+};
+
+var weatherRequest = new OAuth.OAuth(
+    null,
+    null,
+    'dj0yJmk9bzhvUk1mOE1ROEFJJmQ9WVdrOWFGcHhNV2xCTldrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWU5',
+    '0ba03db854bf88ee3de249f2164774ba19d3f8dc',
+    '1.0',
+    null,
+    'HMAC-SHA1',
+    null,
+    header
+);
 
 router.get('/weather', (req, res) => {
     if (req.query.zipcode) {
         // get weather and send 
-        request(`http://dummy.restapiexample.com/api/v1/employees`, { json: true }, (err, response, body) => {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log(body)
-                res.send(body)
+        weatherRequest.get(
+            `https://weather-ydn-yql.media.yahoo.com/forecastrss?location=${req.query.zipcode},ca&format=json`,
+            null,
+            null,
+            function (err, data, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(data)
+                    console.log(result)
+                    res.send(data)
+                }
             }
-
-        })
+        );
     } else {
         res.send("ou have requested weather without specifying zipcode")
     }
